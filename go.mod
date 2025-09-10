382de6fee870791905a399214a8b653963b9bbd7
David Campbell <campbellsan@gmail.com>
2025-09-09 23:04:55 -0700

    feat: minimal Go web service with /healthz and /hello endpoints

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/DavidCampbell-architect/hello-go-service
+
+go 1.25.1
---
+++ main.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
+
+func health(w http.ResponseWriter, r *http.Request) {
+	w.WriteHeader(http.StatusOK)
+	_, _ = w.Write([]byte("ok"))
+}
+
+func hello(w http.ResponseWriter, r *http.Request) {
+	name := r.URL.Query().Get("name")
+	if name == "" {
+		name = "world"
+	}
+	w.Header().Set("Content-Type", "application/json")
+	fmt.Fprintf(w, `{"message":"hello %s"}`, name)
+}
+
+func main() {
+	mux := http.NewServeMux()
+	mux.HandleFunc("/healthz", health)
+	mux.HandleFunc("/hello", hello)
+	addr := ":8080"
+	log.Printf("listening on %s", addr)
+	log.Fatal(http.ListenAndServe(addr, mux))
+}
+
